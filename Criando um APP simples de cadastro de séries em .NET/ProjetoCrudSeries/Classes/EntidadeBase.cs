e170d3871ee165c363bc95a8963f23d602089097
Cristianeasreis <cristianeasreis@hotmail.com>
2021-02-23 18:26:30 -0300

    Estudando app net dio

---
+++ Criando um APP simples de cadastro de séries em .NET/ProjetoCrudSeries/Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace DIO.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Criando um APP simples de cadastro de séries em .NET/ProjetoCrudSeries/Classes/Serie.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+		private Genero Genero { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private int Ano { get; set; }
+        private bool Excluido {get; set;}
+
+        // Métodos
+		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+		{
+			this.Id = id;
+			this.Genero = genero;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.Ano = ano;
+            this.Excluido = false;
+		}
+
+        public override string ToString()
+		{
+			// Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+			return retorno;
+		}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaId()
+		{
+			return this.Id;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+    }
+}
